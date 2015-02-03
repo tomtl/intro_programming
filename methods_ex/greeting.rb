c6401857dd2642e401a969d0ed3e2e6564961df1
Tom Lee <tom_t_lee@yahoo.com>
2015-02-03 20:31:44 +0000

    Methods execises

---
+++ methods_ex/greeting.rb
@@ -0,0 +1,8 @@
+# Write a program that contains the method called greeting that takes a name as
+# its parameter. I then prints a greeting message with the name included in it.
+
+def greeting(name)
+  "Greetings #{name}!"
+end
+
+puts greeting("Joe")
---
+++ methods_ex/multiply.rb
@@ -0,0 +1,8 @@
+# Write a program that includes a method called multiply that takes two 
+# arguments and returns the product of the two numbers.
+
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(2, 3)
